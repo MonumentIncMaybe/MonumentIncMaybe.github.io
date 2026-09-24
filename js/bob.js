8242e8019827327792e4b7b9da872547bd8020af
MonumentIncMaybe <ballbuster4827@gmail.com>
2026-09-23 21:43:00 -0500

    Update bob.js

--- js/bob.js
+++ js/bob.js
@@ -1,42 +1,45 @@
-
 /*
     BOB
-
-    v0.1 temporary database.
-
-    This is deliberately local for now.
-    Later, Monument can ask Moonlight
-    to communicate with the real server.
+    Database layer storing DOORS Entities data.
 */
 
 const Bob = {
 
-    name: "Bob",
+    name: "bob",
+    entityName: "Bob",
+    type: "database",
 
-    data: [
-        {
-            name: "Monument",
-            type: "core"
-        },
-
-        {
-            name: "Moonlight",
-            type: "connection"
-        },
-
-        {
-            name: "Bob",
-            type: "database"
-        }
+    // DOORS entities database
+    entities: [
+        { id: "rush", name: "Rush", danger: "Medium", awake: true },
+        { id: "ambush", name: "Ambush", danger: "High", awake: true },
+        { id: "seek", name: "Seek", danger: "High", awake: true },
+        { id: "figure", name: "Figure", danger: "Extreme", awake: true },
+        { id: "screech", name: "Screech", danger: "Low", awake: true },
+        { id: "eyes", name: "Eyes", danger: "Medium", awake: true },
+        { id: "halt", name: "Halt", danger: "Medium", awake: true },
+        { id: "hide", name: "Hide", danger: "Medium", awake: true },
+        { id: "jack", name: "Jack", danger: "Low", awake: true },
+        { id: "dupe", name: "Dupe", danger: "Medium", awake: true },
+        { id: "guiding_light", name: "Guiding Light", danger: "None", awake: true },
+        { id: "curious_light", name: "Curious Light", danger: "None", awake: true },
+        { id: "bob", name: "Bob", danger: "None", awake: true },
+        { id: "jeff", name: "Jeff", danger: "None", awake: true },
+        { id: "el_goblino", name: "El Goblino", danger: "None", awake: true }
     ],
 
-    async search(query) {
+    // APIs
+    getEntities() {
+        return this.entities;
+    },
 
-        const text = query.toLowerCase();
+    getEntityById(id) {
+        return this.entities.find(e => e.id === id);
+    },
 
-        return this.data.filter(item =>
-            item.name.toLowerCase().includes(text)
-        );
+    isEntityAwake(id) {
+        const entity = this.getEntityById(id);
+        return entity ? entity.awake : false;
     }
 };
 
